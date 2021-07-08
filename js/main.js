d1111b2aa22d732fc27b9d04d9d9b6383b52d591
Rulótico González <rulotico@gmail.com>
2021-07-07 20:23:08 -0500

    JS class
    Vimos las bases de la estructura javascript para crear nuestro bot bebé de conversación.

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Tipos de variables
+
+var texto = "hola como estas"; //texto
+var x = 20; //númericas
+var flag= true; //boolean
+
+
+
+// #maria
+
+var starts=["hola","que onda!","hey","hello"];
+var chat=["a si", ["a","b","c"], "apoco"];
+
+
+function saludar(){
+	alert(starts[1]);
+}
+
+
+
+
+// alert("Hola, si funciona");
+// console.log(texto);
