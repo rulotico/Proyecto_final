00087c4674d46cbf37717df0cc76c09e9e9dfdcf
Rulótico González <rulotico@gmail.com>
2021-07-12 20:11:36 -0500

    ahora mi bot saluda
    Inclui objetos y funciones if

--- js/main.js
+++ js/main.js
@@ -8,14 +8,73 @@ var flag= true; //boolean
 
 // #maria
 
+//arreglos
 var starts=["hola","que onda!","hey","hello"];
 var chat=["a si", ["a","b","c"], "apoco"];
 
+//objetos
 
-function saludar(){
-	alert(starts[1]);
+var user={ 
+	"name":"",
+	"nicknames":"",
+	"age":0,
+	"second_name":"",
+	"auto":{
+		"marca":"seat",
+		"modelo":"leon",
+		"year":"2019",
+		"color":"white"
+	}
+}
+
+//variables globales
+
+var flag_saludo=false;
+var flag_name=false;
+const random = Math.floor(Math.random() * starts.length);
+
+
+function maria(res){
+
+
+	// El saludo
+	if (!flag_saludo) {
+		
+		console.log(starts[random]+", que tal..");
+		flag_saludo=true;
+
+	}else if(flag_name){
+
+		user.name=res;
+		console.log(starts[random]+user.name);
+
+	}else if(!user.name || flag_name==false){
+
+		console.log("Hola, como te llamas?");
+		flag_name=true;
+
+	}
+
+
+
+	// if(!user.name){
+	// 	console.log("Hola, como te llamas?");
+	// }
+
+	// console.log(starts[1]+" "+user.name);
 	//cuando corra la función saludar responda un saludo random
 }
+
+
+
+//Primero saluda ✅
+//Segundo me pregunta mi nombre
+//Tercero me pregunta mi edad
+//Si tengo auto o no?
+//Tienes apodos
+//Despedida. 
+
+
 
 
 
